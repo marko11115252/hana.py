03a777a0e31dc68c87f60a51f5d445ec6e72989c
Speedwagon <35416567+marko11115252@users.noreply.github.com>
2021-04-20 19:13:55 +0200

    Update hana.py

--- hana.py
+++ hana.py
@@ -1,10 +1,52 @@
 import discord
+import random
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+bot = commands.Bot(command_prefix = '.')
 
-@client.event
+#bot ready message
+
+@bot.event
 async def on_ready():
-	print('Bot is ready.')
+	print('Hana-chan is ready to play.')
 
-client.run('ODM0MDc0MjA0NjU0NjY1NzQ5.YH7mIg.lnWjS9NmOQ_h4etXpb5KHQHBr_s')
+#user join message
+
+@bot.event
+async def on_member_join(member):
+	print(f'{member} has joined a server.')	
+
+#user leave message
+
+@bot.event
+async def on_member_remove(member):
+	print(f'{member} has left a server.')
+
+#bot command ping
+
+@bot.command()
+async def ping(ctx):
+	await ctx.send(f'MYA-NEE! {round(bot.latency * 1000)}ms')
+
+#bot command nani
+
+@bot.command()
+async def nani(ctx, *, question):
+	responses = ['MYA-NEE!',
+				 'Issoni asoboio',
+				 '~YAMEROOO!!!!',
+				 'EH?',
+				 'Oi ningen!',
+				 'Bukkorosu!',
+				 'Bukkorostearu!,',
+				 'OI OI OI OI OI!',
+				 'Matte kudasai',
+				 'Nanda kore wa?!',
+				 'Kuso gaki domo',
+				 'Mahou shoujo'
+				 'https://www.youtube.com/watch?v=bydxMFQd3wc']
+	await ctx.send(random.choice(responses)
+
+#token
+
+bot.run('token')
